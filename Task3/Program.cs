8aac7f958c5c86b2291bf3bd7757dede01c1594f
dmitriy.andreev.1995@inbox.ru <dmitriy.andreev.1995@inbox.ru>
2022-08-24 06:09:07 +0300

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+
+Console.WriteLine("цифра дня недели");
+int numberofWeek = Convert.ToInt32(Console.ReadLine());
+
+void CheckThedayOf (int numberofWeek)
+{
+    if(numberofWeek == 6 || numberofWeek == 7)
+{
+    Console.WriteLine("(is this day off)yes,have a good day.");
+}
+else if(numberofWeek < 1 || numberofWeek > 7)
+{
+    Console.WriteLine("This day isn't day off");
+}
+else Console.WriteLine("not,Wake up,Time to work man");
+}
+CheckThedayOf(numberofWeek);
